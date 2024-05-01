6953a539b3741cb8fd02c58b82ec4a9512ea8bf2
Philippe latu <latuphilippe@gmail.com>
2024-05-01 19:09:07 +0200

    Indiviual sensor routes

--- rest_03.py
+++ rest_03.py
@@ -15,21 +15,21 @@ def index():
     return redirect("/api/v1", code=302)
 
 
-@app.route("/api/v1", methods=["get"])
+@app.route("/api/v1", methods=["get"], strict_slashes=False)
 def main():
     main_data = ["sensors", "leds", "messages"]
     return json.dumps(main_data, indent=2)
 
 
-@app.route("/api/v1/sensors", methods=["get"])
+@app.route("/api/v1/sensors", methods=["get"], strict_slashes=False)
 def route_sensors():
     return sensors.get_sensors_data()
 
 
-@app.route("/api/v1/sensors/<name>", methods=["get"])
+@app.route("/api/v1/sensors/<name>", methods=["get"], strict_slashes=False)
 def route_sensors_name(name):
     return sensors.get_sensors_data(name)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080)  # nosec
+    app.run(debug=True, host="::", port=8080)  # nosec
--- sensors_03.py
+++ sensors_03.py
@@ -11,66 +11,80 @@ from sense_hat import SenseHat
 SLEEP_TIME = 200  # in ms
 T_SAMPLES = 10
 
-sensors_data = {"temperature": [], "humidity": [], "pressure": []}
+sensors_data = {"temperature": "", "humidity": "", "pressure": ""}
 
 sense = SenseHat()
 
 
-def read_cpu_temp():
+def _read_cpu_temp():
     res = os.popen("vcgencmd measure_temp").readline()  # nosec
     t = float(res.replace("temp=", "").replace("'C\n", ""))
     return t
 
 
-def read_sense_temp():
+def _read_sense_temp():
     t1 = sense.get_temperature_from_humidity()
     t2 = sense.get_temperature_from_pressure()
     return (t1 + t2) / 2
 
 
-def read_sense_humidity():
+def _read_sense_humidity():
     return sense.get_humidity()
 
 
-def read_sense_pressure():
+def _read_sense_pressure():
     return sense.get_pressure()
 
 
 # Use moving average of 10 measures to compensate CPU heating
-def avg_temp(t):
-    if not hasattr(avg_temp, "lst"):
-        avg_temp.lst = [t] * 10
+def _avg_temp(t):
+    if not hasattr(_avg_temp, "lst"):
+        _avg_temp.lst = [t] * 10
     else:
-        avg_temp.lst = avg_temp.lst[1:]
-        avg_temp.lst.insert(9, t)
-        # print(avg_temp.lst)
-    return sum(avg_temp.lst) / 10
+        _avg_temp.lst = _avg_temp.lst[1:]
+        _avg_temp.lst.insert(9, t)
+    return sum(_avg_temp.lst) / 10
 
 
-def estimate_temp():
+def _estimate_temp():
     for _ in range(T_SAMPLES):
-        t_cpu = read_cpu_temp()
-        t_sense = read_sense_temp()
-        t_est = avg_temp(t_sense - ((t_cpu - t_sense) / 1.5))
+        t_cpu = _read_cpu_temp()
+        t_sense = _read_sense_temp()
+        t_est = _avg_temp(t_sense - ((t_cpu - t_sense) / 1.5))
         time.sleep(SLEEP_TIME / 1000)
     return t_est
 
 
 @dispatch()
 def get_sensors_data():
-    sensors_data["temperature"] = estimate_temp()
-    sensors_data["humidity"] = read_sense_humidity()
-    sensors_data["pressure"] = read_sense_pressure()
+    """Collects temperature, humidity and pressure measures from the Sense HAT.
+
+    Returns:
+        JSON dictionary: temperature, humidity and pressure measures
+    """
+
+    sensors_data["temperature"] = _estimate_temp()
+    sensors_data["humidity"] = _read_sense_humidity()
+    sensors_data["pressure"] = _read_sense_pressure()
+
     return json.dumps(sensors_data, indent=2)
 
 
 @dispatch(str)
 def get_sensors_data(sense):  # noqa: F811
+    """Collects a specific sensor measure from the Sense HAT.
+
+    Args:
+        sense (str): name of the sensor to read
+
+    Returns:
+        JSON dictionary: sensor name key and measure value
+    """
     if sense == "temperature":
-        return json.dumps({"temperature": estimate_temp()}, indent=2)
+        return json.dumps({"temperature": _estimate_temp()}, indent=2)
     elif sense == "humidity":
-        return json.dumps({"humidity": read_sense_humidity()}, indent=2)
+        return json.dumps({"humidity": _read_sense_humidity()}, indent=2)
     elif sense == "pressure":
-        return json.dumps({"pressure": read_sense_pressure()}, indent=2)
+        return json.dumps({"pressure": _read_sense_pressure()}, indent=2)
     else:
-        return json.dumps(sensors_data, indent=2)
+        return json.dumps({"message": "sensor not found!"}, indent=2), 404
